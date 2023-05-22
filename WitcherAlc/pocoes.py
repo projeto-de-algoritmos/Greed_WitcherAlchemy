d9a997e7d966cc54b12a601ec057f416afa28870
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-05-22 17:19:30 -0300

    Update pocoes.py

--- WitcherAlc/pocoes.py
+++ WitcherAlc/pocoes.py
@@ -3,17 +3,37 @@ class Ingrediente:
         self.nome = nome
         self.tamanho = tamanho
         self.propriedades = propriedades
+        self.usado = False
+
+    def usar(self):
+        if not self.usado:
+            self.usado = True
+        else:
+            print(f"O ingrediente {self.nome} já foi usado e não pode ser adicionado a mais de um frasco.")
 
 
 class Frasco:
     def __init__(self, capacidade):
         self.capacidade = capacidade
+        self.espaco_livre = capacidade
         self.ingredientes = []
 
     def adicionar_ingrediente(self, ingrediente):
-        self.ingredientes.append(ingrediente)
+        if ingrediente.usado:
+            print(f"O ingrediente {ingrediente.nome} já foi usado e não pode ser adicionado ao frasco.")
+        elif ingrediente.tamanho <= self.espaco_livre:
+            self.ingredientes.append(ingrediente)
+            self.espaco_livre -= ingrediente.tamanho
+            ingrediente.usar()
+        else:
+            print(f"O frasco não tem espaço suficiente para adicionar o ingrediente {ingrediente.nome}.")
 
     def mostrar_ingredientes(self):
-        for ingrediente in self.ingredientes:
-            print(ingrediente.nome)
+        print(f"Ingredientes no frasco (capacidade: {self.capacidade} - espaço livre: {self.espaco_livre}):")
+        if len(self.ingredientes) > 0:
+            for ingrediente in self.ingredientes:
+                print(ingrediente.nome)
+        else:
+            print("Nenhum ingrediente no frasco.")
+        print()
 
