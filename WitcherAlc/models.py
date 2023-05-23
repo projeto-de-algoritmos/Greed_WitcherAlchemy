9ab28af7d3805c6e061ae1cba0003769c70ec1a3
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-05-23 15:43:27 -0300

    Update models.py

--- WitcherAlc/models.py
+++ WitcherAlc/models.py
@@ -1,18 +1,11 @@
 class Ingrediente:
-    """
-    Classe que representa ingredientes, que possuem uma propriedade interessante para os personagens.
-    Eles também possuem sua quantidade disponível, um tamanho e um valor de importância para aplicação do Knapsack.
-    """
     def __init__(self, nome, tamanho, propriedades):
         self.nome = nome
-        self.tamanho = tamanho
+        self.tamanho = float(tamanho)
         self.propriedades = propriedades
         self.usado = False
 
     def usar(self):
-        """
-        Função que retorna se o ingrediente foi usado ou não (se não, é usado).
-        """
         if not self.usado:
             self.usado = True
         else:
@@ -20,37 +13,27 @@ class Ingrediente:
 
 
 class Frasco:
-    """
-    Classe que representa um frasco que guarda os ingredientes.
-    Possui uma capacidade e guarda o espaço livre e os ingredientes que estão no frasco.
-    """
     def __init__(self, capacidade):
         self.capacidade = capacidade
         self.espaco_livre = capacidade
         self.ingredientes = []
 
     def adicionar_ingrediente(self, ingrediente):
-        """
-        Adiciona um ingrediente ao fraso (verifica tamanho e se já foi usado).
-        """
         if ingrediente.usado:
             print(f"O ingrediente {ingrediente.nome} já foi usado e não pode ser adicionado ao frasco.")
         elif ingrediente.tamanho <= self.espaco_livre:
             self.ingredientes.append(ingrediente)
             self.espaco_livre -= ingrediente.tamanho
             ingrediente.usar()
-        else:
-            print(f"O frasco não tem espaço suficiente para adicionar o ingrediente {ingrediente.nome}.")
+        #else:
+            #print(f"O frasco não tem espaço suficiente para adicionar o ingrediente {ingrediente.nome}.")
 
     def mostrar_ingredientes(self):
-        """
-        Mostra os ingredientes que estão em um frasco.
-        """
         print(f"Ingredientes no frasco (capacidade: {self.capacidade} - espaço livre: {self.espaco_livre}):")
         if len(self.ingredientes) > 0:
             for ingrediente in self.ingredientes:
                 print(ingrediente.nome)
         else:
-            print("Nenhum ingrediente possível no frasco.")
+            print("Nenhum ingrediente no frasco.")
         print()
 
